89cc0609cba28b64114c6c5e6f463f3c9776d789
michaeldriscoll <michaelndriscoll81@gmail.com>
2021-03-31 20:40:33 -0400

    updating

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,37 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
+import styled from "styled-components"
 import React from "react"
+
+const NavBar = styled.div`
+ display: flex;
+ flex-direction: column;
+ padding: 0 20px;
+ @media (min-width: 800px) {
+  justify-content: space-between;
+}
+@media (min-width: 1150px) {
+flex-direction: row;
+justify-content: space-between;
+padding: 20px 15px 0 15px;
+max-width: 1250px;
+}
+`
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: 'rgb(250, 250, 250)',
       marginBottom: `1.45rem`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+    <NavBar>
+        <img src="https://i.imgur.com/3ei96FN.png" alt="logo" className="logo"/>
+          <h1 className="header-links">About</h1>
+          <h1 className="header-links">Our Mission</h1>
+          <h1 className="header-links">Board of Directors</h1>
+          <h1 className="header-links">Contact</h1>
+        </NavBar>
         <Link
           to="/"
           style={{
@@ -24,10 +39,8 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          the
         </Link>
-      </h1>
-    </div>
   </header>
 )
 
